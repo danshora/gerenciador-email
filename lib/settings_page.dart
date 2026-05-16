57fe543487568cd1a8503fe8f7ec726e145e463b
danfsz <103087456+danshora@users.noreply.github.com>
2026-05-15 21:23:22 -0700

    Update settings_page.dart

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../providers/account_manager.dart';
-import '../theme.dart';
+
+// Imports corrigidos para a raiz
+import 'account_manager.dart';
+import 'theme.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
